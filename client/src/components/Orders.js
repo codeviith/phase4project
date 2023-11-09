448bd68320b040706645cc61ef8dfdb63a9d0c6b
Juan Carlos Lopez Larco <jclopezlarco@gmail.com>
2023-11-09 11:21:22 -0500

    works

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -6,7 +6,9 @@ function Order({ user,items }) {
   const [userOrders, setUserOrders]= useState([]);
   const [userOrderID, setUserOrdersID]= useState([]);
   const [orders, setOrders] =useState([])
+  const [itemsInOrder, setItemsInOrder] = useState([])
  
+  let tempArray = [];
 
 
   useEffect(() => {
@@ -58,35 +60,97 @@ function Order({ user,items }) {
 
   function handleOrderClick(order) {
     setSelOrder(order);
+    console.log(order);
+    getItemsID(order);
+  
+
+  }
+  
+  
+  function getItemsID(order) {
+ 
+    for (let i = 0; i < order.order_item.length; i++) {
+      let itemId = order.order_item[i].item_id;
+      tempArray.push(itemId);
+    }
+    setItemsInOrder(tempArray);
+    console.log(tempArray)
   }
 
-  return (
-    <div>
-      <button id="testbutton" onClick={() => test()}>
-        test
-      </button>
-      <button id="getorders" onClick={() => getorders()}>
-        getorders
-      </button>
 
-      <div id="OrderList">
-        {orders.length > 0 ? (
-          orders.map((order) => (
-            <ul key={order.id} onClick={() => handleOrderClick(order)}>
-              <li>Order ID: {order.id}</li>
-              <li>Date Created: {order.created_date}</li>
-              <li>Number of items: {order.n_items}</li>
-              <li>Order price: {order.cost}</li>
-            </ul>
-          ))
-        ) : (
-          <p>Nothing to show yet</p>
-        )}
+
+  function getQuantityForItem(itemID) {
+    const orderItem = selOrder.order_item.find((item) => item.item_id === itemID);
+    return orderItem ? orderItem.quantity : 0;
+  }
+
+
+
+   return (
+      <div>
+        <button id="testbutton" onClick={() => test()}>
+          test
+        </button>
+        <button id="getorders" onClick={() => getorders()}>
+          getorders
+        </button>
+    
+        <div id="OrderList">
+          {orders.length > 0 ? (
+            orders.map((order) => (
+              <ul key={order.id} onClick={() => handleOrderClick(order)}>
+                <li>Order ID: {order.id}</li>
+                <li>Date Created: {order.created_date}</li>
+                <li>Number of items: {order.n_items}</li>
+                <li>Order price: {order.cost}</li>
+              </ul>
+            ))
+          ) : (
+            <p>Nothing to show yet</p>
+          )}
+        </div>
+    
+        <div id="SelectedOrder">
+          {selOrder && selOrder.cost > 0 ? (
+            <div>
+              <h2>Selected order</h2>
+              <ul id="mainOrder">
+                <li>Order ID: {selOrder.id}</li>
+                <li>Date Created: {selOrder.created_date}</li>
+                <li>Number of items: {selOrder.n_items}</li>
+                <li>Order price: {selOrder.cost}</li>
+              </ul>
+              <h3>Items in the order:</h3>
+              {itemsInOrder.map((itemID) => {
+                const item = items.find((item) => item.id === itemID);
+                const quantity = getQuantityForItem(itemID);
+                return (
+                  <div key={itemID}>
+                    <ul>
+                      <li>Item ID: {itemID}</li>
+                      <li>Item Quantity: {quantity}</li>
+                      {/* Additional information for each item */}
+                      {item && (
+                        <div>
+                          <li>Item Image URL: {item.image_url}</li>
+                          <li>Item Name: {item.name}</li>
+                          <li>Item Brand: {item.brand}</li>
+                          <li>Item Price: {item.price}</li>
+                        </div>
+                      )}
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <p>Nothing to show yet</p>
+          )}
+        </div>
+
+
       </div>
-      <div>Selected Order</div>
-
-    </div>
-  );
-}
+    );
+          }
 
 export default Order;
